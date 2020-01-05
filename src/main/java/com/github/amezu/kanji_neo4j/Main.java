e1e03683c9a78402123ebb29112c9d9caa03fb42
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-01-05 15:24:56 +0100

    Change data to kanji

--- src/main/java/com/github/amezu/kanji_neo4j/Main.java
+++ src/main/java/com/github/amezu/kanji_neo4j/Main.java
@@ -52,16 +52,16 @@ public class Main implements AutoCloseable {
         driver.close();
     }
 
-    @RequestMapping(value = {"/greeting", ""})
-    String getAllGreetings(@RequestParam(value = "search", required = false, defaultValue = "") String messagePart, Map<String, Object> model) {
+    @RequestMapping(value = {"/kanji", ""})
+    String getAllKanjis(@RequestParam(value = "search", required = false, defaultValue = "") String reading, Map<String, Object> model) {
         try (Session session = driver.session()) {
             StatementResult result = session.run(
-                    "MATCH (g:Greeting) WHERE g.message STARTS WITH {messagePart} RETURN g.message + ' (' + id(g) + ')' AS message",
-                    parameters("messagePart", messagePart));
+                    "MATCH (k:Kanji) WHERE k.reading STARTS WITH {reading} RETURN k.character + ' (' + id(k) + ')' AS kanji",
+                    parameters("reading", reading));
             ArrayList<String> records = new ArrayList<>();
             while (result.hasNext()) {
                 Record record = result.next();
-                records.add(record.get("message").asString());
+                records.add(record.get("kanji").asString());
             }
             model.put("records", records);
             return "index";
@@ -71,16 +71,16 @@ public class Main implements AutoCloseable {
         }
     }
 
-    @RequestMapping("/greeting/{id}")
-    String getGreeting(@PathVariable int id, Map<String, Object> model) {
+    @RequestMapping("/kanji/{id}")
+    String getKanji(@PathVariable int id, Map<String, Object> model) {
         try (Session session = driver.session()) {
             StatementResult result = session.run(
-                    "MATCH (g:Greeting) WHERE id(g) = {id} RETURN g.message + ' (' + id(g) + ')' AS message",
+                    "MATCH (k:Kanji) WHERE id(k) = {id} RETURN k.character + ' (' + id(k) + ')' AS kanji",
                     parameters("id", id));
             ArrayList<String> records = new ArrayList<>();
             while (result.hasNext()) {
                 Record record = result.next();
-                records.add(record.get("message").asString());
+                records.add(record.get("kanji").asString());
             }
             model.put("records", records);
             return "index";
@@ -90,17 +90,16 @@ public class Main implements AutoCloseable {
         }
     }
 
-    @RequestMapping("/greeting/add")
-    String addGreeting(Map<String, Object> model) {
+    @RequestMapping("/kanji/add")
+    String addKanji(@RequestParam("char") String character, @RequestParam("read") String reading, Map<String, Object> model) {
         try (Session session = driver.session()) {
             final String message = session.writeTransaction(tx -> {
-                StatementResult result = tx.run("CREATE (g:Greeting) " +
-                                "SET g.message = $message " +
-                                "RETURN g.message + ', from node ' + id(g)",
-                        parameters("message", "Hello world!"));
+                StatementResult result = tx.run(
+                        "CREATE (k:Kanji) SET k.character = {character}, k.reading = {reading} RETURN k.character + ', from node ' + id(k)",
+                        parameters("character", character, "reading", reading));
                 return result.single().get(0).asString();
             });
-            model.put("message", message);
+            model.put("message", "Added kanji " + message);
             return "error";
         } catch (Exception e) {
             model.put("message", e.getMessage());
