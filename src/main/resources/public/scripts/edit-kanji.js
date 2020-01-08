30af020016458c1036a06d764171ab815e5de8b7
Julia Zarzycka <36897051+Amezu@users.noreply.github.com>
2020-01-08 19:39:30 +0100

    Add JS for editing kanji

---
+++ src/main/resources/public/scripts/edit-kanji.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function editKanji() {
+    var inputs = document.querySelectorAll("input");
+
+    var reading = inputs[0].value;
+    var strokes = inputs[1].value;
+
+    $.ajax({
+        url: "update?strokes=" + strokes + "&read=" + reading,
+        success: function (result) {
+            alert(result);
+            $("form")[0].reset();
+            location.reload();
+        },
+        error: function (e) {
+            alert(e.responseText);
+            location.reload();
+        }
+    });
+}
+
+document.querySelector("button").addEventListener("click", function (event) {
+    event.preventDefault();
+    editKanji();
+})
